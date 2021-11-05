c46f602a3f6f176db163fc6b3a8cd3b2834b8ee9
Tracks12 <cardinal.florian.erwan@gmail.com>
2021-11-05 09:35:02 +0100

    Add header component & style with Bootstrap

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent implements OnInit {
+	title = 'Todo App';
+	subtitle = 'My first Angular application';
+	bgColor = '#eee';
+	today = new Date();
+
+	constructor() { }
+
+	ngOnInit(): void {
+
+	}
+}
