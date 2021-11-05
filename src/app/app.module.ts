fa1f5b7319d2f7ad1792e093a059fca3bbce8ccb
Tracks12 <cardinal.florian.erwan@gmail.com>
2021-11-05 09:39:41 +0100

    Add task-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule
